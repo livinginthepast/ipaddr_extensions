9d85e332a3ef89e69fbbd04a36c9cce0f416ee51
Eric Saxby <sax@livinginthepast.org>
2014-02-25 23:33:57 -0800

    Bump version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sax@livinginthepast.org'
 license          'MIT'
 description      'Adds the ipaddr_extensions gem to chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 
 recipe "ipaddr_extensions::default", "Installs ipaddr_extensions via chef_gem"
 
